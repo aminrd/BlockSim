f7aee6c7423b4218d1336ef94210c5bfc6cde131
Amin <amin@goldspot.ca>
2021-05-29 19:40:54 -0400

    minor fix; finder continues

--- BlockSim/finder/finder.py
+++ BlockSim/finder/finder.py
@@ -1,72 +1,93 @@
-from BlockSim.orm.database import Account, User, Database, Transaction
+from BlockSim.orm.database import Account, Database, Transaction
 from collections import defaultdict, OrderedDict
 from tqdm import tqdm
+import statistics
 
 
-def binary_find(balance, balances):
-    left_idx, right_idx = 0, len(balances) - 1
+def binary_find(balance, all_balances):
+    """
+    Find all accounts having equal balance to target balance or close balance.
 
-    if balances[0][0] > balance or balances[-1][0] < balance:
+    Parameters
+    ----------
+    balance: float
+        Target balance
+
+    all_balances: list
+        List of tuples balances and ids (balance_i, id_i)
+
+    Returns
+    -------
+    l: list
+        List of accounts of tuples (balance_c, id_c)
+    """
+    left_idx, right_idx = 0, len(all_balances) - 1
+
+    if all_balances[0][0] > balance or all_balances[-1][0] < balance:
         return []
 
     while right_idx > left_idx:
         index = (right_idx + left_idx) // 2
-        sub = balances[index][0]
+        sub = all_balances[index][0]
 
         # Found on left
-        if balances[left_idx][0] == balance:
+        if all_balances[left_idx][0] == balance:
             i, ind = left_idx, []
-            while i <= right_idx and balances[i][0] == balance:
+            while i <= right_idx and all_balances[i][0] == balance:
                 ind.append(i)
                 i += 1
-            return [balances[i] for i in ind]
+            return [all_balances[i] for i in ind]
 
         # Found on middle
         elif sub == balance:
-            i, j, ind = index, index+1, []
+            i, j, ind = index, index + 1, []
 
-            while i >= left_idx and balances[i][0] == balance:
+            while i >= left_idx and all_balances[i][0] == balance:
                 ind.append(i)
                 i -= 1
 
-            while j <= right_idx and balances[j][0] == balance:
+            while j <= right_idx and all_balances[j][0] == balance:
                 ind.append(j)
                 j += 1
 
-            return [balances[i] for i in ind]
+            return [all_balances[i] for i in ind]
 
         # Found on right
-        elif balances[right_idx] == balance:
+        elif all_balances[right_idx] == balance:
             i, ind = right_idx, []
-            while i >= left_idx and balances[i][0] == balance:
+            while i >= left_idx and all_balances[i][0] == balance:
                 ind.append(i)
                 i -= 1
-            return [balances[i] for i in ind]
+            return [all_balances[i] for i in ind]
 
         elif sub > balance:
             if right_idx == index:
                 indices = sorted([right_idx, left_idx])
-                return [balances[ind] for ind in indices]
+                return [all_balances[ind] for ind in indices]
             right_idx = index
         else:
             if left_idx == index:
                 indices = sorted([right_idx, left_idx])
-                return [balances[ind] for ind in indices]
+                return [all_balances[ind] for ind in indices]
             left_idx = index
 
     indices = sorted([right_idx, left_idx])
-    i, j = indices[0] - 1, indices[1]+1
-    v1, v2 = balances[indices[0]-1][0], balances[indices[1]+1][0]
+    i, j = indices[0] - 1, indices[1] + 1
+    v1, v2 = all_balances[indices[0] - 1][0], all_balances[indices[1] + 1][0]
 
-    while i >= 0 and balances[i][0] == v1:
+    while i >= 0 and all_balances[i][0] == v1:
         indices.append(i)
         i -= 1
 
-    while j < len(balances) and balances[j][0] == v2:
+    while j < len(all_balances) and all_balances[j][0] == v2:
         indices.append(j)
         j += 1
 
-    return [balances[i] for i in indices]
+    return [all_balances[i] for i in indices]
+
+
+def confidence_level(target, value):
+    return 1 - abs(value - target) / target
 
 
 class Finder:
@@ -93,7 +114,9 @@ class Finder:
                 if trx.src is not None:
                     balances[c_type][trx.src] += trx.amount
 
-        self.balances = balances
+        self.balances = dict()
+        for crypto_name, arr in balances.items():
+            self.balances[crypto_name] = {k: v for k, v in arr.items() if v > 0}
 
     def find(self, ratios):
         """
@@ -117,16 +140,34 @@ class Finder:
         if sum(v for v in ratios.values()) != 1:
             raise ValueError("Sum of the steak values in ratios should be equal to 1.0!")
 
+        if len(ratios.keys()) < 2:
+            raise ValueError(f"Steak balances should have at least two steaks but given {len(ratios.keys())}")
+
         # Sort coins from smallest number of accounts to largest
-        coin_order = sorted((len(self.balances[c], c)) for c in self.coins)
+        coin_order = sorted((len(self.balances[c]), c) for c in self.coins)
 
         # Sort accounts within each crypto-currency by balance
         sorted_balances = OrderedDict()
         for _, c in coin_order:
-            sorted_balances[c] = sorted((balance, a_id) for a_id, balance in self.balances[c])
+            sorted_balances[c] = sorted((balance, a_id) for a_id, balance in self.balances[c].items())
+
+        # Metric = (accuracy, variance)
+        best_metric = (0, 1000)
+        keys = list(sorted_balances.keys())
+        for acc in sorted_balances[keys[0]]:
+            answer = {keys[0]: [acc, 1]}
+
+            for k_index in range(1, len(keys)):
+                k_prev, k_current = keys[k_index - 1], keys[k_index]
+                
+
+
+
+        pass
 
 
 if __name__ == '__main__':
+    print('Testing BlockSim.finder module')
     from pprint import pprint
     import random
 
@@ -136,5 +177,10 @@ if __name__ == '__main__':
     balances = [(random.randrange(0, 10000), i) for i in range(1000)]
     balances = sorted(balances)
     result = binary_find(10, balances)
+    pprint(result)
+
+    steak = {'Bitcoin': 0.1, 'Ethereum': 0.5, 'Doge': 0.4}
+    f_result = finder.find(steak)
+    pprint(f_result)
 
     print('Done!')
